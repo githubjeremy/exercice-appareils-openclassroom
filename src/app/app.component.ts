9a60e6c7ec81db64b07b842671b6fc587c69b3ce
jedacosta <gremyfasol@gmail.com>
2019-12-30 13:46:06 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blog';
+  isAuth = false;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+  appareils = [
+    {
+      name: 'Machine 1',
+      status: 'éteint'
+    },
+    {
+      name: 'Machine 2',
+      status: 'allumé'
+    },
+    {
+      name: 'Machine 3',
+      status: 'éteint'
+    },
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+   public onAllumer() {
+     this.appareils.forEach(element => {
+       if (element.status === 'éteint') {
+         element.status = 'allumé';
+       }
+     });
+   }
+   public onEteindre() {
+    this.appareils.forEach(element => {
+      if (element.status === 'allumé') {
+        element.status = 'éteint';
+      }
+    });
+  }
+
 }
